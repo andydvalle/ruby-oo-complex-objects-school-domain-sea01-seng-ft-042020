e607ce5c38b031b5ca1db5b98ee62f64a3c85ed6
Andy Del Valle <andydelvalle89@gmail.com>
2020-04-22 18:22:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+require 'pry'
+
+class School 
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = roster
+  end
+  
+  def roster
+    @roster = {}
+  end
+  
+  def add_student (student_name, grade_num)
+    binding.pry
+    @roster[:grade_num] = []
+    @roster[:grade_num] << student_name
+  end
+  
+end
